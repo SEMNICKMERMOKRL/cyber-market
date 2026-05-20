aff672f3e10915c63eab81532fcf2c021791df2f
SEMNICKMERMOKRL <169011602+SEMNICKMERMOKRL@users.noreply.github.com>
2026-05-20 03:55:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from 'cors'
-
 import mercadopago from 'mercadopago'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -14,60 +16,57 @@ app.use(express.json())
 ========================= */
 
 mercadopago.configure({
-  access_token:
-    'APP_USR-6549580196272623-052002-015c76633dd8043e01d4d604c793b816-3415580190',
+  access_token: process.env.MP_ACCESS_TOKEN,
 })
 
 /* =========================
-   GERAR PIX
+   CRIAR PIX
 ========================= */
 
 app.post('/create-pix', async (req, res) => {
   try {
     const { total } = req.body
 
-    const payment =
-      await mercadopago.payment.create({
-        transaction_amount: Number(total),
+    const payment = await mercadopago.payment.create({
+      transaction_amount: Number(total),
 
-        description:
-          'Pedido Rota do Burger',
+      description: 'Pedido Rota do Burger',
 
-        payment_method_id: 'pix',
+      payment_method_id: 'pix',
 
-        payer: {
-          email:
-            'playbr24@gmail.com',
-        },
-      })
+      payer: {
+        email: 'playbr24@gmail.com',
+      },
+    })
 
-    const data =
-      payment.body.point_of_interaction
-        .transaction_data
-
-    res.json({
-      qr_code: data.qr_code,
+    return res.json({
+      qr_code:
+        payment.body.point_of_interaction
+          .transaction_data.qr_code,
 
       qr_code_base64:
-        data.qr_code_base64,
+        payment.body.point_of_interaction
+          .transaction_data.qr_code_base64,
 
-      pixUrl: data.ticket_url,
+      pixUrl:
+        payment.body.point_of_interaction
+          .transaction_data.ticket_url,
     })
   } catch (error) {
     console.log(error)
 
-    res.status(500).json({
+    return res.status(500).json({
       error: 'Erro ao gerar PIX',
     })
   }
 })
 
 /* =========================
-   SERVER
+   START SERVER
 ========================= */
 
 app.listen(3001, () => {
   console.log(
-    'SERVIDOR PIX ONLINE NA PORTA 3001'
+    'SERVIDOR PIX ONLINE -> http://localhost:3001'
   )
 })
